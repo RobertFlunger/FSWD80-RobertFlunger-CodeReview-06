6977dde0b8646f0f9838974e79c768abc5980464
Robert Flunger <robert.flunger@gmx.at>
2019-10-18 23:11:14 +0200

    further tweaks

--- js/script.js
+++ js/script.js
@@ -25,6 +25,7 @@ var Locations = /** @class */ (function () {
     };
     return Locations;
 }());
+/*Create child classes Restaurant and Concert, which both extend Locations*/
 var Restaurant = /** @class */ (function (_super) {
     __extends(Restaurant, _super);
     function Restaurant(locationname, city, zip, address, img, phone, type, web) {
@@ -54,6 +55,7 @@ var Events = /** @class */ (function (_super) {
     };
     return Events;
 }(Locations));
+/*create objects and save them into an array called "places"*/
 var karlschurch = new Locations("St. Charles Church", "Vienna", 1010, "Karlsplatz 1", "img/karlschurch.jpg");
 var viennazoo = new Locations("Vienna Zoo", "Vienna", 1130, "Maxingstraße 13b", "img/zoo.jpg");
 var lemonleaf = new Restaurant("Lemon Leaf", "Vienna", 1050, "Kettenbrückengasse 19", "img/lemonleaf.png", "+43(1)5812308", "Thai", "www.lemonleaf.at");
@@ -61,25 +63,28 @@ var sixta = new Restaurant("Sixta", "Vienna", 1050, "Schönbrunner Straße 21", 
 var kris = new Events("Kris Kristofferson", "Vienna", 1150, "Wiener Stadthalle, Halle F, Roland-Rainer-Platz 1", "img/kristofferson.jpg", "Fr., 15.11.2019", "20:00", 58.50, "http://kriskristofferson.com/");
 var kravitz = new Events("Lenny Kravitz", "Vienna", 1150, "Wiener Stadthalle, Halle D, Roland-Rainer-Platz 1", "img/kravitz.jpg", "Sat., 09.12.2019", "19:30", 47.80, "www.lennykravitz.com/");
 var places = [karlschurch, viennazoo, lemonleaf, sixta, kris, kravitz];
-/*var date = new Date(2019, 10, 18, 14, 50);
-var created = date.getDate() + "." + date.getMonth() + "." + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes();
-/*Created: 24.10.2019 12:45*/
 function randomDate(start, end) {
     return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 }
-// randomDate(new Date (2019, 09, 10), new Date(2019, 09, 20))
-var dateCreated = [];
+/*Dates have been randomly created through the commented out function. I copied their values
+into the array to have the same dates on all pages - with the for-loop in the comment I could randomly
+create them, but then they wouldn't stay the same in all 3 web pages
+randomDate(new Date (2019, 09, 10), new Date(2019, 09, 20))*/
 /*for (let index in places) {
     dateCreated[index] = randomDate(new Date (2019, 09, 10), new Date(2019, 09, 20));
-}
-console.log(dateCreated);*/
+}*/
+var dateCreated = [];
 dateCreated = ["Fri Oct 11 2019 16:26:26 GMT+0200 (Mitteleuropäische Sommerzeit)", "Fri Oct 18 2019 16:42:34 GMT+0200 (Mitteleuropäische Sommerzeit)",
     "Fri Oct 11 2019 11:02:47 GMT+0200 (Mitteleuropäische Sommerzeit)", "Thu Oct 10 2019 19:48:39 GMT+0200 (Mitteleuropäische Sommerzeit)",
     "Sat Oct 12 2019 13:07:04 GMT+0200 (Mitteleuropäische Sommerzeit)", "Fri Oct 11 2019 17:28:51 GMT+0200 (Mitteleuropäische Sommerzeit)"];
+/*.map for the date array so they can be used as data object and be added to the cards in the following loop*/
 var datesArray = dateCreated.map(function (dateString) { return new Date(dateString); });
+/*each object gets a new attribute called "date" with a value from the array*/
 for (var index in places) {
     places[index].date = datesArray[index];
 }
+/*if loop to check for the page and to decide which version to add (default order, ascending, descending sort)
+for the ascending/descending sorts, the corresponding sort function is initiated first, then the elements are added to the page*/
 if (document.getElementById("card-deck")) {
     for (var index in places) {
         document.getElementById("card-deck").innerHTML += places[index].display();
@@ -110,11 +115,3 @@ else if (document.getElementById("card-deck-desc")) {
         document.getElementsByClassName("createdon")[index].innerHTML = "Created on: " + places[index].date.getDate() + "." + places[index].date.getMonth() + "." + places[index].date.getFullYear() + " " + places[index].date.getHours() + ":" + places[index].date.getMinutes();
     }
 }
-/*places.sort(function compare(a,b) {
-    if (a.date > b.date) return 1;
-    if (a.date < b.date) return -1;
-})*/
-/*for (let index in places) {
-    document.getElementById("card-deck-asc").innerHTML += places[index].display();
-}
-*/
--- js/script.ts
+++ js/script.ts
@@ -1,6 +1,5 @@
 // Create parent class locations (Location not possible due to duplicate identifier error)
 
-
 class Locations {
 	locationname : string;
 	city : string;
@@ -34,6 +33,8 @@ class Locations {
 			}
 	}
 
+/*Create child classes Restaurant and Concert, which both extend Locations*/
+
 class Restaurant extends Locations {
 	phone : string;
 	type : string;
@@ -66,7 +67,6 @@ class Restaurant extends Locations {
 					</div>
 				</div>`		
 			}
-
 	}
 
 class Events extends Locations {
@@ -106,7 +106,7 @@ class Events extends Locations {
 
 	}	
 
-
+/*create objects and save them into an array called "places"*/
 var karlschurch = new Locations("St. Charles Church", "Vienna", 1010, "Karlsplatz 1", "img/karlschurch.jpg");
 var viennazoo = new Locations("Vienna Zoo", "Vienna", 1130, "Maxingstraße 13b", "img/zoo.jpg");
 
@@ -118,65 +118,60 @@ var kravitz = new Events("Lenny Kravitz", "Vienna", 1150, "Wiener Stadthalle, Ha
 
 var places = [karlschurch, viennazoo, lemonleaf, sixta, kris, kravitz];
 
-
-/*var date = new Date(2019, 10, 18, 14, 50);
-var created = date.getDate() + "." + date.getMonth() + "." + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes();
-/*Created: 24.10.2019 12:45*/
-
 function randomDate(start, end) {
 	return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
 }
 
-// randomDate(new Date (2019, 09, 10), new Date(2019, 09, 20))
+/*Dates have been randomly created through the commented out function. I copied their values 
+into the array to have the same dates on all pages - with the for-loop in the comment I could randomly
+create them, but then they wouldn't stay the same in all 3 web pages
+randomDate(new Date (2019, 09, 10), new Date(2019, 09, 20))*/
 
-var dateCreated = [];
 /*for (let index in places) {
 	dateCreated[index] = randomDate(new Date (2019, 09, 10), new Date(2019, 09, 20));
-}
-console.log(dateCreated);*/
+}*/
+
+var dateCreated = [];
 
 dateCreated = ["Fri Oct 11 2019 16:26:26 GMT+0200 (Mitteleuropäische Sommerzeit)", "Fri Oct 18 2019 16:42:34 GMT+0200 (Mitteleuropäische Sommerzeit)",
 "Fri Oct 11 2019 11:02:47 GMT+0200 (Mitteleuropäische Sommerzeit)", "Thu Oct 10 2019 19:48:39 GMT+0200 (Mitteleuropäische Sommerzeit)", 
 "Sat Oct 12 2019 13:07:04 GMT+0200 (Mitteleuropäische Sommerzeit)", "Fri Oct 11 2019 17:28:51 GMT+0200 (Mitteleuropäische Sommerzeit)"];
+
+/*.map for the date array so they can be used as data object and be added to the cards in the following loop*/
+
 var datesArray = dateCreated.map( dateString => new Date(dateString));
+
+/*each object gets a new attribute called "date" with a value from the array*/
 
 for (let index in places) {
 	places[index].date = datesArray[index];
 }
 
+/*if loop to check for the page and to decide which version to add (default order, ascending, descending sort)
+for the ascending/descending sorts, the corresponding sort function is initiated first, then the elements are added to the page*/
+
 if (document.getElementById("card-deck")) {
-for (let index in places) {
-	document.getElementById("card-deck").innerHTML += places[index].display();
-	document.getElementsByClassName("createdon")[index].innerHTML = "Created on: " + places[index].date.getDate() + "." + places[index].date.getMonth() + "." + places[index].date.getFullYear() + " " + places[index].date.getHours() + ":" + (places[index].date.getMinutes()<10?'0':'') + places[index].date.getMinutes();
-}} else if (document.getElementById("card-deck-asc")) {
+	for (let index in places) {
+		document.getElementById("card-deck").innerHTML += places[index].display();
+		document.getElementsByClassName("createdon")[index].innerHTML = "Created on: " + places[index].date.getDate() + "." + places[index].date.getMonth() + "." + places[index].date.getFullYear() + " " + places[index].date.getHours() + ":" + (places[index].date.getMinutes()<10?'0':'') + places[index].date.getMinutes();
+	}
+} else if (document.getElementById("card-deck-asc")) {
 	places.sort(function compare(a,b) {
 		if (a.date > b.date) return 1;
 		if (a.date < b.date) return -1;
-		})
-		for (let index in places) {
+		});
+
+	for (let index in places) {
 		document.getElementById("card-deck-asc").innerHTML += places[index].display();
 		document.getElementsByClassName("createdon")[index].innerHTML = "Created on: " + places[index].date.getDate() + "." + places[index].date.getMonth() + "." + places[index].date.getFullYear() + " " + places[index].date.getHours() + ":" + places[index].date.getMinutes();
 	}
-
 } else if (document.getElementById("card-deck-desc")) {
 	places.sort(function compare(a,b) {
 		if (a.date < b.date) return 1;
 		if (a.date > b.date) return -1;
-		})
-		for (let index in places) {
+		});
+	for (let index in places) {
 		document.getElementById("card-deck-desc").innerHTML += places[index].display();
 		document.getElementsByClassName("createdon")[index].innerHTML = "Created on: " + places[index].date.getDate() + "." + places[index].date.getMonth() + "." + places[index].date.getFullYear() + " " + places[index].date.getHours() + ":" + places[index].date.getMinutes();
 	}		
 }
-
-
-/*places.sort(function compare(a,b) {
-	if (a.date > b.date) return 1;
-	if (a.date < b.date) return -1;
-})*/
-
-/*for (let index in places) {
-	document.getElementById("card-deck-asc").innerHTML += places[index].display();
-}
-*/
-
